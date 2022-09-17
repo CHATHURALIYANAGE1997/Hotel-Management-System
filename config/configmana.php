094d567c6b68529fb9657d099f811c72090142ba
CHATHURALIYANAGE1997 <chathurabimalka@gmail.com>
2022-09-17 10:50:16 +0530

    Update configmana.php

---
+++ config/configmana.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'manager');
+define('DB_PASSWORD', 'mana123');
+define('DB_NAME', 'hotel_de_asiana');
+ 
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD );
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
